e45946ab31594bf9c46efd817c4977792a024574
kavita <kaviyadav0499@gmail.com>
2023-09-09 20:35:06 +0530

    firstcommit

---
+++ src/employee/management/system/Conn.java
@@ -0,0 +1,28 @@
+
+
+package employee.management.system;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection con;
+    Statement st;
+
+    public Conn () 
+    {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeemanagementsystem?useSSL=false",
+							"root", "root");
+            st = con.createStatement();
+			
+			
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
